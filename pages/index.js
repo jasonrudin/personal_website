277002a425c6ad0ec1ce6b0b7edd3dcde631b14b
jasonrudin <j.b.rudin@gmail.com>
2022-03-21 16:23:28 -0400

    updated link styling on home page

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className = "text-zinc-800 font-ss max-w-xl min-h-screen mx-auto flex flex-col justify-center p-6">
+    <div className = "text-zinc-700 font-ss max-w-xl min-h-screen mx-auto flex flex-col justify-center p-6">
       <Head>
         <title>Jason Rudin</title>
         <meta name="description" content="Jason Rudin's corner of the internet."/>
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className = "">
         <header>
-          <h1 className = "text-2xl">Hi, I'm Jason</h1>
+          <h1 className = "text-4xl">Hi, I'm Jason</h1>
           <h2 className = "text-zinc-500 text-lg">I build digital products in New York City.</h2>
         </header>
       </main>
@@ -30,7 +30,7 @@ export default function Home() {
         <div>
           <ul className = "m-0">
             <li className = "flex flex-row items-baseline justify-between">
-              <div className = "">Idea Explorer, ???</div>
+              <div className = "">Starting Something New™</div>
               <div className = "text-sm font-light text-zinc-500 justify-self-end min-w-[100px]">2022 — </div>
             </li>
             <li className = "flex flex-row items-baseline justify-between my-2">
@@ -55,12 +55,12 @@ export default function Home() {
 
       <section className = "my-2">
         <h3 className = "uppercase font-light text-sm text-zinc-500">The other stuff</h3>
-        <p>Check out <Link href = "/books"><a className = "underline">books</a></Link> I've read and some <Link href = "/art"><a className = "underline">art</a></Link> I made. I also built my dining room table. It doesn't wobble.</p>
+        <p>Check out <Link href = "/books"><a className = "underline underline-offset-2 decoration-zinc-300 hover:decoration-zinc-700 transition duration-75">books</a></Link> I've read and some <Link href = "/art"><a className = "underline underline-offset-2 decoration-zinc-300 hover:decoration-zinc-700 transition duration-75">art</a></Link> I made. I also built my dining room table. It doesn't wobble.</p>
       </section>
 
       <section className = "mt-2 mb-4">
         <h3 className = "uppercase font-light text-sm text-zinc-500">Contact Info</h3>
-        <p>Say hi on <a className = "underline" href = "https://twitter.com/jasonrudin">Twitter</a>, <a className = "underline" href = "https://www.linkedin.com/in/jasonrudin/">LinkedIn</a>, or email me at j.b.rudin [at] gmail [dot] com.</p>
+        <p>Say hi on <a className = "underline underline-offset-2 decoration-zinc-300 hover:decoration-zinc-700 transition duration-75" href = "https://twitter.com/jasonrudin">Twitter</a>, <a className = "underline underline-offset-2 decoration-zinc-300 hover:decoration-zinc-700 transition duration-75" href = "https://www.linkedin.com/in/jasonrudin/">LinkedIn</a>, or email me at j.b.rudin [at] gmail [dot] com.</p>
       </section>
 
       <footer className = "text-xs font-light text-zinc-500 min-w-150">Last updated: February 24, 2022</footer>

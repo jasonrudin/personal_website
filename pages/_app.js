1dc8e59ec8a6ae8834f5d7ab4a962d7b5c3e1aba
jasonrudin <j.b.rudin@gmail.com>
2022-04-04 13:16:02 -0400

    added local env file to hopefully make GA work

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
 import '../styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import Script from "next/script";
+
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script id='ga-analytics'>
+        {
+          `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `
+        }
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
